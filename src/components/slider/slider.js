7b2bc7e5f85706b0bc949698ecb9b8111297e26f
Ageychev Denis <ageychevdenis@gmail.com>
2022-05-25 12:24:49 +0900

    Added coverUrl

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,38 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
-
-import useKinopoiskService from '../../services/use-kinopoisk-server';
-
-import img from '../../assets/img/slider/slide_3.jpg';
+import SwiperCore, { Navigation, Autoplay } from 'swiper';
 
 import './slider.scss';
 import 'swiper/swiper-bundle.css';
 
 const Slider = () => {
-   const [movieList, setMovieList] = useState([]);
-
-   const { getMovieSlide } = useKinopoiskService();
-
    const gallery = [
-      { id: 1234853, title: 'Мистер Нокаут' }
+      { id: 1234853, title: 'Мистер Нокаут', discription: 'Упрямый тренер и любимая девушка помогают юноше стать чемпионом. Драма о советском боксере Валерии Попенченко', coverUrl: 'https://avatars.mds.yandex.net/get-ott/2419418/2a0000017fbb370187bdd97e7b9c214801d3/orig', logoUrl: "https://avatars.mds.yandex.net/get-ott/239697/2a0000017fbb371d8375424be3a56b7022fb/orig" },
+      { id: 1100777, title: 'Триггер', discription: 'Страшная трагедия вынуждает психолога-провокатора вернуться к практике. Напряженная драма с Максимом Матвеевым', coverUrl: 'https://avatars.mds.yandex.net/get-ott/374297/2a0000017e08446283517541047b63d35a54/orig', logoUrl: "https://avatars.mds.yandex.net/get-ott/236744/2a00000180bf38e7506037453dd1e1c11cc2/orig" },
+      { id: 4642708, title: 'Нулевой пациент', discription: 'Врач в Элисте сталкивается с первой вспышкой ВИЧ в СССР. Сериал про людей перед лицом неизвестности', coverUrl: 'https://avatars.mds.yandex.net/get-ott/1531675/2a00000180bd38dbb078d0ef9cd5bf42b28b/orig', logoUrl: "https://avatars.mds.yandex.net/get-ott/1652588/2a00000180bd38ef2b4671712f895bd624a3/orig" }
    ]
 
-   useEffect(() => {
-
-      getMovieSlide(1234853)
-         .then(onMovieListLoaded)
-      //eslint-disable-next-line
-   }, [])
-
-   const onMovieListLoaded = (movieList) => {
-      setMovieList(movieList);
-   }
-
-   console.log(movieList);
-
-   SwiperCore.use(Navigation);
+   SwiperCore.use([Navigation, Autoplay]);
 
    return (
       <section className="slider">
@@ -44,31 +25,25 @@ const Slider = () => {
                      prevEl: '.slider__buttons-prev',
                      nextEl: '.slider__buttons-next'
                   }}
+                  loop={true}
+                  autoplay={{
+                     delay: 4000,
+                     disableOnInteraction: true
+                  }}
+                  speed={500}
                >
                   <div className="slider__wrapper">
-                     <SwiperSlide>
-                        <Link className='slider__slide' to='/movie/1234853' >
-                           <img src={movieList.coverUrl} alt="Мистер Нокаут" />
-                           <div className="slider__slide-body">
-                              <h3>Мистер Нокаут</h3>
-                              <p>Упрямый тренер и любимая девушка помогают юноше стать чемпионом. Драма о советском боксере Валерии Попенченко</p>
-                           </div>
-                        </Link>
-                     </SwiperSlide>
-                     <SwiperSlide>
-                        <Link className='slider__slide' to='/'>
-                           <img src={img} alt="" />
-                           <div className="slider__slide-body">
-                              <h3>Мистер Нокаут</h3>
-                              <p>Упрямый тренер и любимая девушка помогают юноше стать чемпионом. Драма о советском боксере Валерии Попенченко</p>
-                           </div>
-                        </Link>
-                     </SwiperSlide>
-                     <SwiperSlide>
-                        <Link className='slider__slide' to='/'>
-                           <img src={img} alt="" />
-                        </Link>
-                     </SwiperSlide>
+                     {gallery.map(slide => (
+                        <SwiperSlide>
+                           <Link className='slider__slide' key={slide.id} to={`/movie/${slide.id}`} >
+                              <img className='slider__slide-img' src={slide.coverUrl} alt={slide.title} />
+                              <div className="slider__slide-body">
+                                 <img className="slider__slide-title" src={slide.logoUrl} alt={slide.title} />
+                                 <p>{slide.discription}</p>
+                              </div>
+                           </Link>
+                        </SwiperSlide>
+                     ))}
                   </div>
                </Swiper>
                <button className="slider__buttons-prev swiper-button-prev"></button>
